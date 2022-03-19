748fb536750c83b9b2daa147748eb3a16cdadec0
LexieHolder <lexieannholder@gmail.com>
2022-03-18 21:58:40 -0400

    working on connecting notes library to notes.js to server.js

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+
 
 
 
 
 
 app.listen(PORT, () => {
-  console.log('API server now on port' + ${PORT} +'!')
+  console.log(`API server now on port ${PORT}!`);
 });
