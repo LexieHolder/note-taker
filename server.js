b566066fd7d22d77f21db7a4389e16d4be2b5a86
LexieHolder <lexieannholder@gmail.com>
2022-03-18 22:15:19 -0400

    updated notes.js to record createNote, export to db notes array, need fix, add function validate?

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//connects to public folder
+app.use(express.static('public'));
+//sets up for post requests
+app.use(express.urlencoded({ extended: true }));
+//middleware function parsing incoming requests
+app.use(express.json());
+
+
 
 
 
